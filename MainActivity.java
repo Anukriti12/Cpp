f20197502c2dd7b23e5efa8c060adedb814c3402
KUNAL FARMAH <38591331+KunalFarmah98@users.noreply.github.com>
2018-06-01 00:26:31 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,60 @@
+package com.apps.kunalfarmah.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int noofcoffees=1;
+
+    public void submitOrder(View view) {
+
+        display(noofcoffees);
+        displayPrice(noofcoffees*5);
+        Toast.makeText(getApplicationContext(),"THANK YOU", Toast.LENGTH_LONG).show();
+    }
+
+    public void increment(View view){
+        display(++noofcoffees);
+    }
+
+    public void decrement(View view){
+        display(--noofcoffees);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+
+}
